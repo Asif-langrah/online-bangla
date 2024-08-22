120cefba6ee4a792cec1b910151b3d79773cbcad
asif-langrah <ak.development.pk@gmail.com>
2024-08-22 07:43:05 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Image from 'next/image';
 const page = () => {
   return (
     <div><figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-    <img className="w-24 h-24 rounded-full mx-auto" src="/asif-pic.jpeg" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 rounded-full mx-auto" src="/asif-pic.jpeg" alt="" width="384" height="512"/>
     <div className="pt-6 text-center space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
